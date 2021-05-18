18f67813474837f08de829479235a4408a020dd4
DenysenkoStas <49059676+denysenkostas@users.noreply.github.com>
2021-05-18 13:13:40 +0300

    Search

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -14,7 +14,8 @@ export default class App extends Component {
             this.createTodoItem('Drink Coffee'),
             this.createTodoItem('Make Awesome App'),
             this.createTodoItem('Have a lunch')
-        ]
+        ],
+        term: ''
     };
 
     createTodoItem(label) {
@@ -83,8 +84,24 @@ export default class App extends Component {
         }));
     };
 
+    // Search
+    onSearchChange = (term) => {
+        this.setState({term});
+    };
+
+    search(items, term) {
+        if (term.length === 0) {
+            return items;
+        }
+
+        return items.filter(item => {
+            return item.label.toLowerCase().indexOf(term.toLowerCase()) > -1;
+        });
+    }
+
     render() {
-        const {todoData} = this.state;
+        const {todoData, term} = this.state;
+        const visibleItems = this.search(todoData, term);
         const doneCount = todoData.filter(el => el.done).length;
         const todoCount = todoData.length - doneCount;
 
@@ -92,11 +109,11 @@ export default class App extends Component {
             <div className='todo-app'>
                 <AppHeader toDo={todoCount} done={doneCount}/>
                 <div className='top-panel'>
-                    <SearchPanel/>
+                    <SearchPanel onSearchChange={this.onSearchChange}/>
                     <ItemStatusFilter/>
                 </div>
                 <TodoList
-                    todos={todoData}
+                    todos={visibleItems}
                     onDeleted={this.deleteItem}
                     onToggleImportant={this.onToggleImportant}
                     onToggleDone={this.onToggleDone}
--- src/components/SearchPanel/SearchPanel.jsx
+++ src/components/SearchPanel/SearchPanel.jsx
@@ -1,16 +1,29 @@
+import {Component} from 'react';
 import {TextField} from '@material-ui/core';
 import './SearchPanel.scss';
 
-const SearchPanel = () => {
-    return (
-        <TextField
-            className='search-panel'
-            label='Type to search'
-            color='primary'
-            variant='outlined'
-            size='small'
-        />
-    );
-};
+export default class SearchPanel extends Component {
+    state = {
+        term: ''
+    };
 
-export default SearchPanel;
+    onSearchChange = (e) => {
+        const term = e.target.value;
+        this.setState({term});
+        this.props.onSearchChange(term);
+    };
+
+    render() {
+        return (
+            <TextField
+                className='search-panel'
+                label='Type to search'
+                color='primary'
+                variant='outlined'
+                size='small'
+                value={this.state.term}
+                onChange={this.onSearchChange}
+            />
+        );
+    }
+}
